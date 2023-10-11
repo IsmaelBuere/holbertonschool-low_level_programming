b0f2a0d6c3e50419a221c027e0fcb1176c6d5ee0
IsmaelBuere <7674@holbertonstudents.com>
2023-10-11 10:41:32 -0700

    c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_aplhabet_x10 - repeats the print 10 time
+ * Return: void
+ */
+void print_aplhabet_x10(void)
+{
+	int z;
+	char o;
+
+	for (z = 0; z < 10; ++)
+	{
+		for (o = 'a'; o <= 'z'; o++)
+		{
+			_putchar(o);
+		}
+		_putchar('\n');
+	}
+}
