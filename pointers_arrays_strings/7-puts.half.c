985582bce6dc432a5ef131e68afeed3aefd2ff00
IsmaelBuere <7476@holbertonstudents.com>
2023-10-18 15:41:37 -0700

    p

---
+++ pointers_arrays_strings/7-puts.half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int i = 0, leng = 0, n;
+
+	while (str[i++])
+		leng++;
+
+	if ((leng % 2) == 0)
+		n = leng / 2;
+
+	else 
+		n = (leng + 1) / 2;
+
+	for (i = n; i < leng; i++)
+		_putchar(str[i];
+
+	_putchar('\n')
+}
