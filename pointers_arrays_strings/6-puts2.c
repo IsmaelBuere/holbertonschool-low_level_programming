7098eae732f0228576e1148d95a3fdeef2e7b472
IsmaelBuere <7476@holbertonstudents.com>
2023-10-18 09:50:24 -0700

    p

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,14 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (i >= 0)
 	{
-		putchar(str[i]);
+		if (str[i] == '\0')
+		{
+			_putchar('\0');
+		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
-	putchar('\n');
 }
