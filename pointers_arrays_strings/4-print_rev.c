07c3db98e074d7c3f832ad7c561ff3921cb0b224
IsmaelBuere <7476@holbertonstudents.com>
2023-10-18 08:08:42 -0700

    p

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int leng = 0;
+
+	while (s[leng] != '\0')
+	{
+		leng++;
+	}
+	
+	for (int i = leng - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n')
+}
