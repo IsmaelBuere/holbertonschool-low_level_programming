ca90896d32aad4df2b6384f4c56857297e02b621
IsmaelBuere <7476@holbertonstudents.com>
2023-10-18 15:56:24 -0700

    p

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
+ *print_array - print an inputted number of elements
+ *@a: the array of integers.
+ *@n: the number of elemts to be printed.
+ *Return: void
  */
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
