c9447bf18920b79ebf395011588eabdde72515fc
IsmaelBuere <7476@holbertonstudents.com>
2023-10-19 09:56:16 -0700

    p

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return s;
-		}
-		s++;
-	}
+	unsigned int i = 0;
 
-	return NULL;
+	for( *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
