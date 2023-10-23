b1ecab9beda0a325e47bb94ba9dbd792adeba7cd
IsmaelBuere <7476@holbertonstudents.com>
2023-10-23 06:28:30 -0700

    p

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,10 @@
 #include "main2.h"
 /**
+ *_strcmp - function that compares two strings
+ *@s1: value to evaluate
+ *@s2: value to evaluate
  *
- *
- *
+ *Return: s1 - *s2
  */
 int _strcmp(const char *s1, const char *s2)
 {
@@ -12,5 +14,5 @@ int _strcmp(const char *s1, const char *s2)
 		s2++;
 	}
 	
-	return *(unsigned char *)s1 - *(unsigned char *)s2;
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
