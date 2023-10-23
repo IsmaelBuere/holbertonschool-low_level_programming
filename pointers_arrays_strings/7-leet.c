7c87f5767fb31510954b6e92aa556268d2a85cbb
IsmaelBuere <7476@holbertonstudents.com>
2023-10-23 07:10:53 -0700

    p

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,37 +1,23 @@
 #include "main2.h"
 /**
+ * leet - Encodes a string to 1337.
+ * @str: value to evaluate
  *
- *
- *
+ * Return: void
  */
 char *leet(char *str)
 {
-	char *r = str;
+	int r1 = 0, r2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (*r)
+	while (str[r1])
 	{
-		if ((*r == 'a') && (*r == 'A'))
+		for (r2 = 0; r2 <= 7; r++)
 		{
-			*r = '4';
+			if (str[r1] == leet[r2] ||
+			    str[r1] - 32 == leet[r2])
+				str[r1] = r2 + '0';
 		}
-		else if ((*r == 'e') && (*r == 'E'))
-		{
-			*r = '3';
-		}
-		else if ((*r == 'o') && (*r == 'O'))
-		{
-			*r = '0';
-		}
-		else if ((*r == 't') && (*r == 'T'))
-		{
-			*r = '7';
-		}
-		else if ((*r == 'l') && (*r == 'L'))
-		{
-			*r = '1';
-		}
-		r++;
+
+		r1++;
 	}
-	
-	return str;
-}
