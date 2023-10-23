0d129cc070f6976df78d2ade8c45544129b1f3f2
IsmaelBuere <7476@holbertonstudents.com>
2023-10-23 07:08:36 -0700

    p

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,23 +10,23 @@ char *leet(char *str)
 
 	while (*r)
 	{
-		if ((*r == 'a') || (*r == 'A'))
+		if ((*r == 'a') && (*r == 'A'))
 		{
 			*r = '4';
 		}
-		else if ((*r == 'e') || (*r == 'E'))
+		else if ((*r == 'e') && (*r == 'E'))
 		{
 			*r = '3';
 		}
-		else if ((*r == 'o') || (*r == 'O'))
+		else if ((*r == 'o') && (*r == 'O'))
 		{
 			*r = '0';
 		}
-		else if ((*r == 't') || (*r == 'T'))
+		else if ((*r == 't') && (*r == 'T'))
 		{
 			*r = '7';
 		}
-		else if ((*r == 'l') || (*r == 'L'))
+		else if ((*r == 'l') && (*r == 'L'))
 		{
 			*r = '1';
 		}
