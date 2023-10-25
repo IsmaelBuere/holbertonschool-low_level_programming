542fcdc982942b6cd961ca8eb74c406898c1f5ea
IsmaelBuere <7476@holbertonstudents.com>
2023-10-25 07:15:37 -0700

    r

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,22 +1,44 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *
- *
- *
+ * is_divisible - tests whether the number is divisible
+ * @n: the number being tested
+ * @div: the divisor
+ * Return: result
  */
-int _sqrt_recursion(int n)
+int is_divisible(int n, int div)
 {
-	if (n < 0)
+	if (n % div == 0)
 	{
-		return (-1);
+		return (0);
 	}
-	if (n == 0 || n == 1)
+
+	if (div == n / 2)
 	{
-		retrun (n);
+		return (1);
 	}
-	else
+
+	return (is_divisible(n, div + 1));
+}
+
+/**
+ * is_prime_number - is the integer a prime number
+ * @n: the number being tested
+ * Return: result
+ */
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
 	{
-		return (find_sqrt(n, 1));
+		return (0);
 	}
-}	
+
+	if (n <= 3)
+	{
+		return (1);
+	}
+
+	return (is_divisible(n, div));
+}
