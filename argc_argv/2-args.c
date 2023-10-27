12754e790cdf11f5e2496992fff0e8ddc29dfbe2
IsmaelBuere <7476@holbertonstudents.com>
2023-10-27 05:55:40 -0700

    p

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
- */
+ * main - a program that prints all arguments it receives.
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }
