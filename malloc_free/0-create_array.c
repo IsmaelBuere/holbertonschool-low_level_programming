a6bc804a77a95a91e614ae09d2b060af168bd901
IsmaelBuere <7674@holbertonstudents.com>
2023-11-01 06:11:18 -0700

    m

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,23 +4,26 @@
  *
  *
  */
-char *create_array(unsigned int size, char c) {
-	if (size == 0)
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size ==0)
 	{
-	return NULL;
+		return (NULL);
 	}
-
-	char *array = (char *)malloc(size * sizeof(char));
-	if (array == NULL)
-    	{
-	return NULL;
-	}
-
-	for (unsigned int i = 0; i < size; i++)
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
 	{
-	array[i] = c;
+	return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
 	}
 
-	return array;
+	return (str);
 }
-
