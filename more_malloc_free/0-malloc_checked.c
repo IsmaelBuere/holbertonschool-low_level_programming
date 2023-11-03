e41ffd7ffbecc71a1be4ac3109b5c85bef94f613
IsmaelBuere <7674@holbertonstudents.com>
2023-11-03 12:36:23 -0700

    m

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ *malloc_checked - allocates memory
+ *@b: bytes to allocate
  *
- *
- *
- *
+ *Return: (atr)
  */
 void *malloc_checked(unsigned int b)
 {
