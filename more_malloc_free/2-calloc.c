728e0b9018c0211d8927d1634a57e0e0d6febc06
IsmaelBuere <7674@holbertonstudents.com>
2023-11-03 13:12:56 -0700

    m

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *_calloc - allocates memory for an array
+ *@nmemb: elements
+ *@size: bytes size of array
+ *Return: (m)
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *m;
+	char *f;
+	unsigned int i;
+
 	if (nmemb == 0 || size == 0)
-	{
-	return (NULL);
-	}
-
-	void *ptr = malloc(nmemb * size);
-
-	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 
-	unsigned int total_size = nmemb * size;
-	unsigned char *byte_ptr = (unsigned char *)ptr;
+	m = malloc(size * nmemb);
 
-	for (unsigned int i = 0; i < total_size; i++)
-	{
-		byte_ptr[i] = 0;
-	}
-	return (ptr);
+	if (m == NULL)
+		return (NULL);
+
+	filler = m;
+
+	for (i = 0; i < (size * nmemb); i++)
+		f[i] = '\0';
+
+	return (m);
 }
