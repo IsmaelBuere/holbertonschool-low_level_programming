2ed650484052f60087687c5d1b8d789b69acee79
IsmaelBuere <7674@holbertonstudents.com>
2023-11-06 09:09:04 -0800

    s

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,11 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *
- *
+ *new_dog - create a new dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *Return: (d1)
  *
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -34,7 +37,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d1);
 		return ('\0');
 	}
-	for (i = 0; i<= ol; i++)
+	for (i = 0; i <= ol; i++)
 		d1->owner[i] = owner[i];
 	return (d1);
 }
