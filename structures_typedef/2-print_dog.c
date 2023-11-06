47a5da3aea436bc50a83604a9d94595bc63d8aa2
IsmaelBuere <7674@holbertonstudents.com>
2023-11-06 08:36:22 -0800

    s

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,13 +2,13 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ *print_dog - prints a struct dog
+ *@d: pointer to an element of type dog
+ *Return: void
  *
  */
 void print_dog(struct dog *d)
-{	
+{
 	if (d == NULL)
 		;
 	else
