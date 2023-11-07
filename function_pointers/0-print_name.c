f516f35bcd0f2b6f661db2221978ef0bd03c36de
IsmaelBuere <7674@holbertonstudents.com>
2023-11-07 08:02:13 -0800

    f

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  *print_name - print a name
  *@name: name of the person
@@ -6,7 +7,7 @@
  */
 void print_name(char *name, void (*f)(char*))
 {
-	if (name == NULL && f == NULL)
+	if (name != NULL && f != NULL)
 		return;
 	f(name);
 }
