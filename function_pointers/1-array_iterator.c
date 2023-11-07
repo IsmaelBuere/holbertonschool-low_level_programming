75d8e65056aed990294d19ad906dd9df89e7e461
IsmaelBuere <7674@holbertonstudents.com>
2023-11-07 08:15:49 -0800

    f

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		return;
 	}
 
-	for (size_t i = 0; i < size; i++)
+	while (size_t i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
