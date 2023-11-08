3e63b6ce881af2528e9a1e2706dfd36084354f72
IsmaelBuere <7674@holbertonstudents.com>
2023-11-08 12:19:06 -0800

    f

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,41 @@
+#inlcude "3-calc.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+int op_div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	else {
+		return (0);
+	}
+}
+int op_mod(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	else
+	{
+		return (0);
+	}
+}
