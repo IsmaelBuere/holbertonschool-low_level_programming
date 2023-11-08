19a16da3f9f5cb8efda9a8b5a7e31be66cb5f0cd
IsmaelBuere <7674@holbertonstudents.com>
2023-11-08 12:42:21 -0800

    f

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -42,8 +42,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b != 0)
-		return (a / b);
+	return (a / b);
 }
 /**
  * op_mod - Returns the remainder of the division of two numbers.
