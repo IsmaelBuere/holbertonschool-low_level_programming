b54701748ccfbd1851b47ec4778cf4b61470bbb3
IsmaelBuere <7674@holbertonstudents.com>
2023-11-08 13:26:57 -0800

    v

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- *
- *
- *
- *
+ *print_strings - print strings followed by a new line
+ *@separator: string to be printed
+ *@n: number of string
+ *@...: number of strings printed
+ *Return: void
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
