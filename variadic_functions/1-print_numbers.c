6ecda5a34d239512aa9271094d2f4d0bd712db84
IsmaelBuere <7674@holbertonstudents.com>
2023-11-08 13:14:40 -0800

    v

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,9 +10,9 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	va_start(args, n);
+	unsigned int i;
 
-	unsigned int i = 0;
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
