8085157a630e6c4a73fe94508323225c40d20a71
IsmaelBuere <7674@holbertonstudents.com>
2023-11-08 14:24:51 -0800

    v

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,8 +1,8 @@
 #include "variadic_functions.h"
-/*
- *print_all - function that prints anything
- *@format: list of types of arguments
- *Return: void
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
