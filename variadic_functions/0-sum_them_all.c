7cbec4b44cb34d191ae03495fa2da5ea6f2c2f1b
IsmaelBuere <7674@holbertonstudents.com>
2023-11-08 12:55:40 -0800

    v

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,13 +8,10 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
 
 	unsigned int sum, i = 0;
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
@@ -23,5 +20,6 @@ int sum_them_all(const unsigned int n, ...)
 	}
 
 	va_end(args);
+
 	return (sum);
 }
