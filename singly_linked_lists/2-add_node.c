e6a06998da459a521a3420dcb4bc74112c600605
IsmaelBuere <7674@holbertonstudents.com>
2023-11-22 13:53:32 -0800

    s

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	list_node_t *new_node = malloc(sizeof(list_node_t));
+
+	if(new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->data = strdup(str);
+
+	if (new_node->data == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = (*head);
+
+	(*head) = new_node;
+
+	return (*head);
+}
