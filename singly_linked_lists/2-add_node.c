0a00f75dcb12b87b8d0d8873cce64124f68a2a80
IsmaelBuere <7674@holbertonstudents.com>
2023-11-22 13:55:56 -0800

    s

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	list_node_t *new_node = malloc(sizeof(list_node_t));
+	list_t *new_node = malloc(sizeof(list_t));
 
 	if(new_node == NULL)
 	{
