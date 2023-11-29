278f68789f15c78325f9cc9a6e9d30a22fd0c939
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2023-11-29 11:53:08 -0300

    bit_manipulation

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int num, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
- count = (num >> index) & 1;
+count = (num >> index) & 1;
 
 	return  (count);
 }
