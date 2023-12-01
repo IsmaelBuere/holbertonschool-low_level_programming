1a18d0d49963e1a5ca751523e6bc346c1697334b
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2023-12-01 10:34:08 -0300

    file_io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,34 +1,50 @@
 #include "main.h"
 /**
+ * _strlen - Returns the length of a string.
+ * @s: String to count.
+ *
+ * Return: (c)
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
+/**
  * create_file - Create a function that creates a file.
- * @filename: Name of the file to create.
- * @text_content: NULL terminated string to write to the file.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ *
  * Return: (1)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int c, tmp;
-	size_t r_w;
+	int r_w, file;
 
-	if (filename == NULL)
-		return (-1);
-	c = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (c == -1)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		tmp = strlen(text_content);
-
-		r_w = write(c, text_content, tmp);
+		r_w = write(file, text_content, _strlen(text_content));
 
 		if (r_w == -1)
 		{
-			write(STDERR_FILENO, "fails", 6);
-			close(c);
+			close(file);
 			return (-1);
 		}
 	}
-	close(c);
+	if (close(file) == -1)
+		return (-1);
+
 	return (1);
 }
