a2d0100cc8cc9f91558b27295ce8615ef2300ef8
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2023-12-01 10:32:03 -0300

    file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,11 +21,11 @@ int create_file(const char *filename, char *text_content)
 		tmp = strlen(text_content);
 
 		r_w = write(c, text_content, tmp);
-		
+
 		if (r_w == -1)
 		{
 			write(STDERR_FILENO, "fails", 6);
-			close(c)
+			close(c);
 			return (-1);
 		}
 	}
