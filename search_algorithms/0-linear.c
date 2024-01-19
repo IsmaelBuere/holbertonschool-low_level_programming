78f8312c88fdc31103fbf77731f35ac0d313c455
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-19 20:59:28 -0300

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -20,7 +20,7 @@ int linear_search(int *array, size_t size, int value)
     for (i = 0; i < size; ++i)
     {
 
-        printf("Comparing value %d with array[%lu] = %d\n", value, i, array[i]);
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
  
         if (array[i] == value)
